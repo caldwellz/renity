b97f405b67462765ca54fa9f75d7ea16d6bdc7f3
Zach Caldwell <zach.caldwell101@gmail.com>
2021-11-23 12:31:31 -0800

    Implement Sprite class

---
+++ src/Sprite.cc
@@ -0,0 +1,246 @@
+/***************************************************
+* Sprite.h: Stateful texture-based sprite class    *
+* Copyright (C) 2021 by Zach Caldwell              *
+****************************************************
+* This Source Code Form is subject to the terms of *
+* the Mozilla Public License, v. 2.0. If a copy of *
+* the MPL was not distributed with this file, You  *
+* can obtain one at http://mozilla.org/MPL/2.0/.   *
+***************************************************/
+
+#include "Sprite.h"
+
+#include <errno.h>
+#include <fenv.h>
+#include <math.h>
+
+#include "config.h"
+#include "types.h"
+#include "Texture.h"
+
+const double radiansToDegrees = 180.0 / 3.141593;
+
+namespace renity {
+struct Sprite::Impl {
+    /** Defaults to full-screen VSynced mode at the native screen resolution. */
+    Impl() {
+        tex = nullptr;
+        scale = Dimension2Dd(1.0, 1.0);
+        direction = Point2Dd(0.0, 1.0); // 0 degrees
+        x = 0.0;
+        y = 0.0;
+        rotation = 0.0;
+        speed = 1.0;
+        usingDefaultOrigin = true;
+        flipHorizontal = false;
+        flipVertical = false;
+    }
+
+    void updateImageOrigin() {
+        if (usingDefaultOrigin) {
+            // Use the default SDL origin (dstrect.w / 2, dstrect.h / 2)
+            origin.x(destRect.width() / 2);
+            origin.y(destRect.height() / 2);
+        }
+    }
+
+    renity::Texture* tex;
+    Point2Di origin;
+    Dimension2Dd scale;
+    Rect2Di srcRect, destRect;
+    Point2Dd direction;
+    double x, y, rotation, speed;
+    bool usingDefaultOrigin, flipHorizontal, flipVertical;
+};
+
+RENITY_API Sprite::Sprite() {
+    // Initialize with default settings
+    pimpl_ = new Impl();
+}
+
+ 
+RENITY_API Sprite::Sprite(Texture& texture) {
+    pimpl_ = new Impl();
+    setTexture(&texture);
+}
+
+
+RENITY_API Sprite::~Sprite() {
+    delete pimpl_;
+}
+
+
+RENITY_API void Sprite::setTexture(Texture* texture) {
+    pimpl_->tex = texture;
+
+    // Reset the image scale and clipping
+    if (pimpl_->tex) {
+        setImageScale(pimpl_->scale);
+        pimpl_->srcRect.x(0);
+        pimpl_->srcRect.y(0);
+        pimpl_->srcRect.size(pimpl_->tex->getSize());
+    }
+}
+
+
+RENITY_API Dimension2Dd Sprite::getImageScale() const {
+    return pimpl_->scale;
+}
+
+
+RENITY_API void Sprite::setImageScale(const Dimension2Dd& scale) {
+    pimpl_->scale = scale;
+    if (pimpl_->tex) {
+        Dimension2Di imageSize = pimpl_->tex->getSize();
+        pimpl_->destRect.width(scale.width() * imageSize.width());
+        pimpl_->destRect.height(scale.height() * imageSize.height());
+        pimpl_->updateImageOrigin();
+    }
+}
+
+
+RENITY_API Rect2Di Sprite::getImageClip() const {
+    return pimpl_->srcRect;
+}
+
+
+RENITY_API void Sprite::setImageClip(const Rect2Di& sourceClip) {
+    pimpl_->srcRect = sourceClip;
+}
+
+
+RENITY_API Point2Di Sprite::getImageOrigin() const {
+    return pimpl_->origin;
+}
+
+
+RENITY_API void Sprite::setImageOrigin(const Point2Di& origin) {
+    pimpl_->usingDefaultOrigin = false;
+    pimpl_->origin = origin;
+}
+
+
+RENITY_API void Sprite::useDefaultOrigin() {
+    pimpl_->usingDefaultOrigin = true;
+    pimpl_->updateImageOrigin();
+}
+
+
+RENITY_API double Sprite::getImageRotation() const {
+    return pimpl_->rotation;
+}
+
+
+RENITY_API void Sprite::setImageRotation(const double& angle) {
+    pimpl_->rotation = angle;
+}
+
+
+RENITY_API void Sprite::flipImageHorizontal() {
+    pimpl_->flipHorizontal = !pimpl_->flipHorizontal;
+}
+
+
+RENITY_API void Sprite::flipImageVertical() {
+    pimpl_->flipVertical = !pimpl_->flipVertical;
+}
+
+
+RENITY_API void Sprite::unflipImage() {
+    pimpl_->flipHorizontal = false;
+    pimpl_->flipVertical = false;
+}
+
+
+RENITY_API Point2Di Sprite::getPosition() const {
+    return pimpl_->destRect.position();
+}
+
+
+RENITY_API void Sprite::setPosition(const Point2Di& position) {
+    pimpl_->destRect.position(position);
+    pimpl_->x = position.x();
+    pimpl_->y = position.y();
+}
+
+
+RENITY_API double Sprite::getMoveHeading() const {
+    // atan2 normally takes (y, x), but we reverse them for the sake of going clockwise
+    double angle = atan2(pimpl_->direction.x(), pimpl_->direction.y()) * radiansToDegrees;
+    if (angle < 0.0)
+        angle += 360.0;
+
+    // Clear any domain errors that may have happened (i.e. passing (0, 0) to atan2).
+    // The expectation is that it will simply have returned 0 in that case.
+    if (math_errhandling & MATH_ERRNO)
+        errno = 0;
+    if (math_errhandling & MATH_ERREXCEPT)
+        feclearexcept(FE_ALL_EXCEPT);
+
+    return angle;
+}
+
+
+RENITY_API void Sprite::setMoveHeading(const double& angle) {
+    // x = sine & y = cosine so that we end up going clockwise from the top.
+    pimpl_->direction.x(sin(angle / radiansToDegrees));
+    pimpl_->direction.y(cos(angle / radiansToDegrees));
+}
+
+
+RENITY_API Point2Dd Sprite::getMoveDirection() const {
+    return pimpl_->direction;
+}
+
+
+RENITY_API void Sprite::setMoveDirection(const Point2Dd& direction) {
+    pimpl_->direction = direction;
+}
+
+
+RENITY_API double Sprite::getMoveSpeed() const {
+    return pimpl_->speed;
+}
+
+
+RENITY_API void Sprite::setMoveSpeed(const double& speed) {
+    pimpl_->speed = speed;
+}
+
+
+RENITY_API void Sprite::bounceHorizontal(bool flipImage) {
+    if (flipImage)
+        flipImageHorizontal();
+
+    // Reflect across the Y axis by negating the angle, effectively (360-angle)
+    double angle = getMoveHeading();
+    setMoveHeading(-angle);
+}
+
+
+RENITY_API void Sprite::bounceVertical(bool flipImage) {
+    if (flipImage)
+        flipImageVertical();
+
+    // Reflect across the X axis
+    double angle = getMoveHeading();
+    setMoveHeading(180 - angle);
+}
+
+
+RENITY_API void Sprite::move() {
+    pimpl_->x += pimpl_->direction.x() * pimpl_->speed;
+    pimpl_->y += pimpl_->direction.y() * pimpl_->speed;
+    pimpl_->destRect.position(Point2Di((int) pimpl_->x, (int) pimpl_->y));
+}
+
+
+RENITY_API bool Sprite::draw() {
+    if (pimpl_->tex) {
+        return pimpl_->tex->draw(&pimpl_->srcRect, &pimpl_->destRect, pimpl_-> \
+        rotation, &pimpl_->origin, pimpl_->flipHorizontal, pimpl_->flipVertical);
+    }
+
+    return false;
+}
+}
---
+++ tests/Sprite.cc
@@ -0,0 +1,139 @@
+/***************************************************
+* Test - Stateful texture-based sprite class       *
+* Copyright (C) 2020-2021 by Zach Caldwell         *
+****************************************************
+* This Source Code Form is subject to the terms of *
+* the Mozilla Public License, v. 2.0. If a copy of *
+* the MPL was not distributed with this file, You  *
+* can obtain one at http://mozilla.org/MPL/2.0/.   *
+***************************************************/
+
+#include "Sprite.h"
+
+#include <assert.h>
+
+#include <physfs.h>
+#include <SDL2/SDL.h>
+#include "Texture.h"
+#include "Window.h"
+#include "resources/epic_png_zip.h"
+
+int main(int argc, char* argv[])
+{
+    // Initialization and basic sanity checks
+    PHYSFS_init(argv[0]);
+    assert(PHYSFS_isInit());
+    assert(PHYSFS_mountMemory(epicArchive, epicArchiveLen, NULL, epicArchiveName, NULL, 1));
+    assert(SDL_Init(SDL_INIT_VIDEO) == 0);
+    renity::Window window;
+    window.useFullscreen(false, false);
+    window.size(renity::Dimension2Di(160, 160));
+    assert(window.open() && window.isOpen());
+    renity::Texture tex(window, epicFile);
+    assert(tex.isValid());
+
+    // Check constructors and setTexture
+    renity::Sprite defSprite;
+    assert(!defSprite.draw());
+    defSprite.setTexture(&tex);
+    assert(defSprite.draw());
+    defSprite.setTexture(nullptr);
+    assert(!defSprite.draw());
+    renity::Sprite sprite(tex);
+    assert(sprite.draw());
+    assert(window.update());
+
+    // Check image scaling
+    renity::Dimension2Dd scale = sprite.getImageScale();
+    assert(1.0 == scale.width());
+    assert(1.0 == scale.height());
+    scale.width(2.0);
+    scale.height(3.0);
+    sprite.setImageScale(scale);
+    scale = sprite.getImageScale();
+    assert(2.0 == scale.width());
+    assert(3.0 == scale.height());
+
+    // Check image clipping
+    renity::Rect2Di clip = sprite.getImageClip();
+    assert(0 == clip.x());
+    assert(0 == clip.y());
+    assert(epicWidth == clip.width());
+    assert(epicHeight == clip.width());
+    sprite.setImageClip(renity::Rect2Di(10, 20, epicWidth + 10, epicWidth + 20));
+    clip = sprite.getImageClip();
+    assert(10 == clip.x());
+    assert(20 == clip.y());
+    assert(epicWidth + 10 == clip.width());
+    assert(epicHeight + 20 == clip.height());
+
+    // Check image rotational origin
+    renity::Point2Di origin = sprite.getImageOrigin();
+    printf("(%i, %i) -> (%i, %i), (%i, %i)\n", epicWidth / 2, epicHeight / 2, origin.x(), origin.y(), clip.width(), clip.height());
+    fflush(NULL);
+    assert(origin.x() == epicWidth / 2);
+    assert(origin.y() == epicHeight / 2);
+    sprite.setImageOrigin(renity::Point2Di(17, 25));
+    origin = sprite.getImageOrigin();
+    assert(origin.x() == 17);
+    assert(origin.y() == 25);
+    sprite.useDefaultOrigin();
+    origin = sprite.getImageOrigin();
+    assert(origin.x() == epicWidth / 2);
+    assert(origin.y() == epicHeight / 2);
+
+    // Check image rotation
+    assert(0.0 == sprite.getImageRotation());
+    sprite.setImageRotation(45.5);
+    assert(45.5 == sprite.getImageRotation());
+
+    // Check position get/set
+    renity::Point2Di position = sprite.getPosition();
+    assert(0 == position.x());
+    assert(0 == position.y());
+    sprite.setPosition(renity::Point2Di(15, 20));
+    position = sprite.getPosition();
+    assert(15 == position.x());
+    assert(20 == position.y());
+
+    // Check heading get/set
+    assert(0.0 == sprite.getMoveHeading());
+    sprite.setMoveHeading(180.0);
+    assert(180.0 == sprite.getMoveHeading());
+
+    // Check direction get/set
+    renity::Point2Dd direction = sprite.getMoveDirection();
+    assert(0.0 == direction.x());
+    assert(-1.0 == direction.y());
+
+    // Check heading/direction interchangeability
+
+
+    // Check speed get/set
+
+
+    // Check bounces
+
+
+    // Check move()
+
+
+    // Display test
+    renity::Rect2Di destTop(41, 0, epicWidth, epicHeight);
+    renity::Rect2Di destBottom(41, epicHeight + 2, epicWidth, epicHeight);
+    for (Uint8 i = 0; i < 30; ++i) {
+        sprite.move();
+        assert(sprite.draw());
+        SDL_SetRenderDrawColor(window.getRenderer(), 0, 0, 255, SDL_ALPHA_OPAQUE); // Pure blue
+        window.update();
+        SDL_Delay(50);
+    }
+
+    // Clean up
+    tex.unload();
+    window.close();
+    SDL_Quit();
+    PHYSFS_deinit();
+
+    return 0;
+}
